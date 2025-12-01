293ba7f8e01f55e81f6df0762d816289b74f5655
Michał Midor <contact@midor.me>
2025-12-01 13:51:13 +0100

    fixed helpers, finished nodes.hpp, ready for nodes.cpp implementation

---
+++ include/config.hpp
@@ -0,0 +1,1 @@
+// for conditional files/methods includes
--- include/package.hpp
+++ include/package.hpp
@@ -5,65 +5,66 @@
 #include "types.hpp"
 #include <set>
 
-namespace NetSim
-{
-    /**
-     * @brief Class representing a product that's being sent in the net
-     */
-    class Package
-    {
-    public:
-        /**
-         * @brief Default constructor
-         */
-        Package();
+namespace NetSim {
+/**
+ * @brief Class representing a product that's being sent in the net
+ */
+class Package {
+public:
+  /**
+   * @brief Default constructor
+   */
+  Package();
 
-        /**
-         * @brief Constructor with a given ID
-         */
-        explicit Package(ElementID id);
+  /**
+   * @brief Constructor with a given ID
+   */
+  explicit Package(ElementID id);
 
-        /**
-         * @brief Disable Copy constructor
-         * It's prohibited due to copying IDs problems - it cannot happend
-         */
-        Package(const Package &) = delete;
-        
-        /**
-         * @brief Disable Copy operator
-         */
-        Package& operator=(const Package &) = delete;
+  /**
+   * @brief Disable Copy constructor
+   * It's prohibited due to copying IDs problems - it cannot happend
+   */
+  Package(const Package &) = delete;
 
-        /**
-         * @brief Moving constructor
-         * 'Noexcept' promises that this function will never throw an exception
-         * Moving constructor and moving operators should always be 'noexcept'
-         */
-        Package(Package &&) noexcept; // && is moving semantics, prevents copying
+  /**
+   * @brief Disable Copy operator
+   */
+  Package &operator=(const Package &) = delete;
 
-        /**
-         * @brief Moving operator
-         * This object can be safely moved
-         */
-        Package& operator=(Package &&) noexcept; // && is moving semantics, prevents copying
+  /**
+   * @brief Moving constructor
+   * 'Noexcept' promises that this function will never throw an exception
+   * Moving constructor and moving operators should always be 'noexcept'
+   */
+  Package(Package &&) noexcept; // && is moving semantics, prevents copying
 
-        /**
-         * @brief Read package ID
-         * @return ElementID package serial number
-         */
-        ElementID get_id() const;
+  /**
+   * @brief Moving operator
+   * This object can be safely moved
+   */
+  Package &
+  operator=(Package &&) noexcept; // && is moving semantics, prevents copying
 
-        /**
-         * @brief Desctructor
-         */
-        ~Package();
+  /**
+   * @brief Read package ID
+   * @return ElementID package serial number
+   */
+  ElementID get_id() const;
 
-    private:
-        ElementID id_;
+  /**
+   * @brief Desctructor
+   */
+  ~Package();
 
-        // ID Poll
-        static std::set<ElementID> assigned_ids_; // 'static' shares this one place in memory between all instances of this object
-        static std::set<ElementID> freed_ids_;
-    };
+private:
+  ElementID id_;
+
+  // ID Poll
+  static std::set<ElementID>
+      assigned_ids_; // 'static' shares this one place in memory between all
+                     // instances of this object
+  static std::set<ElementID> freed_ids_;
+};
 
 } // namespace NetSim
